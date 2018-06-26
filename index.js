e45bc577134028eda9d1219fc4f35da986ac9043
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 18:49:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function findMinAndRemove(array){
   let min = [array[0],0];
   for (let i = 0; i < array.length; i++) {
-    if (item < min) {
-      min = item;
+    if (array[i] < min[0]) {
+      min = [array[i],i];
     }
   }
-  
+
 }
 
 function insertionSort(array){
